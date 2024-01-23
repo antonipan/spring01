cddad33e300d4ecf00708a53d58a2543a6fbc47e
antonio <a.s.panferov@mail.ru>
2024-01-23 17:15:37 +0300

    create new project

---
+++ spring03users/src/test/java/ru/antonio/spring03users/Spring03usersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.antonio.spring03users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring03usersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
